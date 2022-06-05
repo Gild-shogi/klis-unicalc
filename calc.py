3bff29692d390232f3bb633b1add77e3aed03b5b
Gild-Shogi <ok2336.coffee@gmail.com>
2022-06-06 00:28:54 +0900

    add total

--- calc.py
+++ calc.py
@@ -2,7 +2,21 @@ import csv
 # 科目情報のCSV
 subpath = ["./csv/science.csv", "./csv/system.csv", "./csv/resource.csv"]
 # 自分の履修状況のCSV
-userpath = ["./usr/aa.csv", "./usr/ab.csv", "./usr/ac.csv", "./usr/sa.csv", "./usr/sb.csv", "./usr/sc.csv"]
+#userpath = ["./usr/aa.csv", "./usr/ab.csv", "./usr/ac.csv", "./usr/sa.csv", "./usr/sb.csv", "./usr/sc.csv"]
+total = "./usr/total.csv"
+def usr_read_csv(path):
+    mylist = []
+    with open(path, encoding="utf-8") as f:
+        reader = csv.reader(f)
+        content = []
+        for row in reader:
+            #print(content)
+            if len(row) > 3:
+                content.append(row[3])
+        #content = [row[3] for row in reader]
+
+    return content
+
 def read_csv(path):
     mylist = []
     with open(path, encoding="utf-8") as f:
@@ -34,52 +48,58 @@ def science(path, mylist):
     content = read_csv(path[0])
     num = 0.0
     for sci in mylist:
+      if len(sci) > 2:
         if sci[2] == "6":
-            #print(sci)
-            for i in content:
-                #print(list(i[0]), list(sci))
-                if sci == i[0][:-1]:
-                    #print(i[2])
-                    num = num + float(i[2][:-1])
+          for i in content:
+            #print(list(i[0]), list(sci))
+            if sci == i[0][:-1]:
+              #print(i[2])
+              num = num + float(i[2][:-1])
     return num
 def system(path, mylist):
     content = read_csv(path[1])
     num = 0.0
     for sci in mylist:
+      if len(sci) > 2:
+        #print(sci[2]=="7")
         if sci[2] == "7":
-            for i in content:
-                #print(list(i[0]), list(sci))
-                if sci == i[0][:-1]:
-                    #print(i[2])
-                    num = num + float(i[2][:-1])
+          for i in content:
+            #print(sci, i[0])
+            
+            if sci == i[0][:-1]:
+              #print(i[2])
+              num = num + float(i[2][:-1])
     return num
 def resource(path, mylist):
     content = read_csv(path[2])
     num = 0.0
     for sci in mylist:
+      if len(sci) > 2:
         if sci[2] == "8":
-            #print(sci)
-            for i in content:
-                #print(list(i[0]), list(sci))
-                if sci == i[0][:-1]:
-                    #print(i[2])
-                    num = num + float(i[2][:-1])
+          for i in content:
+            #print(list(i[0]), list(sci))
+            if sci == i[0][:-1]:
+              #print(i[2])
+              num = num + float(i[2][:-1])
     return num
-mylist = return_id(userpath)
-#print(mylist)
-sci = science(subpath, mylist)
-sys = system(subpath, mylist)
-res = resource(subpath, mylist)
 
-idx = int(input("自分の主専攻に応じて番号を入力してください\n1:知識科学\n2:システム\n3:資源\n"))
-print(f"専門科目の合計単位数: {sci + sys + res}")
+if __name__ == "__main__":
+    mylist = usr_read_csv(total)
+    #mylist = return_id(total)
+    #print(mylist)
+    sci = science(subpath, mylist)
+    sys = system(subpath, mylist)
+    res = resource(subpath, mylist)
 
-if idx == 1:
-    print(f"他主専攻の単位数：{sys + res}")
-    print(f"残り：{8-sys-res}")
-if idx == 2:
-    print(f"他主専攻の単位数：{sci + res}")
-    print(f"残り：{8-sci-res}")
-if idx == 3:
-    print(f"他主専攻の単位数：{sys + sci}")
-    print(f"残り：{8-sys-sci}")
+    idx = int(input("自分の主専攻に応じて番号を入力してください\n1:知識科学\n2:システム\n3:資源\n"))
+    print(f"専門科目の合計単位数: {sci + sys + res}")
+
+    if idx == 1:
+        print(f"他主専攻の単位数：{sys + res}")
+        print(f"残り：{8-sys-res}")
+    if idx == 2:
+        print(f"他主専攻の単位数：{sci + res}")
+        print(f"残り：{8-sci-res}")
+    if idx == 3:
+        print(f"他主専攻の単位数：{sys + sci}")
+        print(f"残り：{8-sys-sci}")
